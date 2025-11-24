6a4288ef4ef152445d8a37fc6a761b86956b0802
mariasjoholm <maria.frida.sjoholm@outlook.com>
2025-11-24 14:46:38 +0100

    Update Main.java

--- src/main/java/lottorad/Main.java
+++ src/main/java/lottorad/Main.java
@@ -4,6 +4,10 @@ package lottorad;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("hello");
+    	
+    	Model model = new Model(); 
+    	View view = new View(); 
+    	Controller controller = new Controller(model,view); 
+        
     }
 }
