e7329f6eba48a56e771a2a4a44b5e1a6adbaa5ea
mariasjoholm <maria.frida.sjoholm@outlook.com>
2025-11-24 14:53:37 +0100

    Update Main.java

--- src/main/java/lottorad/Main.java
+++ src/main/java/lottorad/Main.java
@@ -7,7 +7,7 @@ public class Main {
     	
     	Model model = new Model(); 
     	View view = new View(); 
-    	Controller controller = new Controller(model,view); 
+    	Controller controller = new Controller (model,view); 
         
     }
 }
